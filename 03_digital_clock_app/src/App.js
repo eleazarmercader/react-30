9228b492237bf34a8f0808b8555fe1c2011b4ab6
eleazarmercader <elmercaderdesundberg@gmail.com>
2023-11-30 13:25:24 +0100

    Digital Clock App

---
+++ 03_digital_clock_app/src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [time, updateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      updateTime(new Date());
+    }, 1000);
+    
+    return () => clearInterval(timer);
+  }, []);
+  
+  return (
+    <div className="App">
+      <div className="elementcontainer">
+        <h1>Digital Clock</h1>
+        <div className="timeparent">
+          <div className="timecontainer">
+            <span className="time">{time.toLocaleTimeString()}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
